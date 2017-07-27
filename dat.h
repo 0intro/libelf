d0f39cd69baf4bbc94c81a8f3dc75f92070ee32d
David du Colombier <0intro@gmail.com>
2017-07-27 23:27:12 +0200

    add osabi enum

--- dat.h
+++ dat.h
@@ -378,6 +378,30 @@ enum {
 };
 
 /*
+ * Operating system/ABI identification
+ */
+enum {
+	ELFOSABI_NONE		= 0,	/* No extensions or unspecified */
+	ELFOSABI_HPUX		= 1,	/* Hewlett-Packard HP-UX */
+	ELFOSABI_NETBSD		= 2,	/* NetBSD */
+	ELFOSABI_GNU		= 3,	/* GNU */
+	ELFOSABI_SOLARIS	= 6,	/* Sun Solaris */
+	ELFOSABI_AIX		= 7,	/* AIX */
+	ELFOSABI_IRIX		= 8,	/* IRIX */
+	ELFOSABI_FREEBSD	= 9,	/* FreeBSD */
+	ELFOSABI_TRU64		= 10,	/* Compaq TRU64 UNIX */
+	ELFOSABI_MODESTO	= 11,	/* Novell Modesto */
+	ELFOSABI_OPENBSD	= 12,	/* Open BSD */
+	ELFOSABI_OPENVMS	= 13,	/* Open VMS */
+	ELFOSABI_NSK		= 14,	/* Hewlett-Packard Non-Stop Kernel */
+	ELFOSABI_AROS		= 15,	/* Amiga Research OS */
+	ELFOSABI_FENIXOS	= 16,	/* The FenixOS highly scalable multi-core OS */
+	ELFOSABI_CLOUDABI	= 17,	/* Nuxi CloudABI */
+	ELFOSABI_OPENVOS	= 18,	/* Stratus Technologies OpenVOS */
+	/* 64-255 Architecture-specific value range */
+};
+
+/*
  * Special Section Indexes
  */
 enum {
